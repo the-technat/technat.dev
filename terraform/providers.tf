cb6e00da84b47e8ec629d87438f6188ed33f702e
Nathanael Liechti <technat@technat.ch>
2023-08-26 17:28:38 +0200

    chore: add some comments

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,13 +1,13 @@
 provider "openstack" {
   application_credential_secret = var.openstack_token
-  application_credential_id     = "4e7a622b013b49f7875628b3a8d2543d"
+  application_credential_id     = "4e7a622b013b49f7875628b3a8d2543d" # generated within the console & unrestricted
   auth_url                      = "https://api.pub1.infomaniak.cloud/identity"
   region                        = "dc3-a"
 }
 
 provider "tailscale" {
-  api_key = var.tailscale_api_key
-  tailnet = "the-technat.github"
+  api_key = var.tailscale_api_key # expires every 90 days
+  tailnet = "the-technat.github"  # created by sign-up via Github
 }
 
 provider "akeyless" {
