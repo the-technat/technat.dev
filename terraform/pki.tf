ce4264a36ad4de115e1f49ef785b2768e9c90f7e
Nathanael Liechti <technat@technat.ch>
2023-08-27 18:12:58 +0200

    feat: add root ca

--- terraform/pki.tf
+++ terraform/pki.tf
@@ -1,7 +1,28 @@
+locals {
+	pki_tags = ["axiom"]
+
+}
+
 resource "akeyless_dfc_key" "primary" {
   alg               = "RSA4096"
   name              = "axiom/ca/primary"
   delete_protection = true
   description       = "Primary Key for Axiom CA"
-  tags              = ["axiom"]
+  tags              = local.pki_tags
+}
+
+resource "akeyless_pki_cert_issuer" "primary" {
+  name = "axiom/ca/primary_ca"
+  description = "Root CA for axiom"
+  signer_key_name = akeyless_dfc_key.primary.name
+  ttl = 2592000 # 30 days
+  delete_protection = true
+
+  allow_any_name = true
+  allow_subdomains = true
+  country = "Switzerland"
+  province = "Bern" 
+  organizations = "the-technat"
+
+  tags = local.pki_tags
 }
