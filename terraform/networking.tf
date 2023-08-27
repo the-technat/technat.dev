0d6b279697b87e9ee45bd916c9135a0351512f90
Nathanael Liechti <technat@technat.ch>
2023-08-27 18:27:11 +0200

    feat: add openstack networking

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -16,3 +16,25 @@ resource "tailscale_dns_nameservers" "tailnet" {
     "2606:1a40:1::38",
   ]
 }
+
+### Openstack Networking
+resource "openstack_networking_network_v2" "axiom" {
+  name           = "axiom"
+  admin_state_up = "true"
+}
+
+resource "openstack_networking_router_v2" "primary" {
+  name                = "primary"
+  external_network_id = "0f9c3806-bd21-490f-918d-4a6d1c648489" # ext-floating1
+}
+
+resource "openstack_networking_subnet_v2" "axiom" {
+  network_id = openstack_networking_network_v2.axiom.id
+  cidr       = "192.168.111.0/24"
+  ip_version = 4
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface_1" {
+  router_id = openstack_networking_router_v2.primary.id
+  subnet_id = openstack_networking_subnet_v2.axiom.id
+}
